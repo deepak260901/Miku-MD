533715aa05911f2617a8761b3357542e4d44ce4b
deepak260901 <105791403+deepak260901@users.noreply.github.com>
2022-11-08 08:24:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['919015286428']
+global.OwnerNumber = ['919015286428']
+global.ownertag = ['919015286428']
+global.BotName = "Gappu"
+global.packname = "『ɪɴᴅ』☯▓🌛🎗🎗🅿🅰🎋𓀎"
+global.author = "Gappu(🪔)🤗💫"
+global.OwnerName = "ᴹᴿ᭄ ❦︎ᴅᴇᴇᴘᴀᴋ-ᴏғcོ ×፝֟͜×"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
@@ -23,7 +23,7 @@ global.sessionName = "session"
 global.prefa = ['-'] // Prefix ---  change it to anythong you want except @//
 
 
-global.location = "West Bengal, India"
+global.location = "India, Bihar, Delhi"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
